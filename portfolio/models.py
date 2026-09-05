399845b0f1479dba04bcdeb778f6f922623afe86
Aldomar Assolin <assolinaldomar@gmail.com>
2026-09-05 11:36:11 -0300

    feat: configura admin e restringe perfil a registro unico

--- portfolio/models.py
+++ portfolio/models.py
@@ -3,6 +3,11 @@ from django.db import models
 
 class Perfil(models.Model):
     """Modelo de perfil do usuario."""
+    id = models.PositiveBigIntegerField(
+        primary_key=True,
+        default=1,
+        editable=False
+    )
     nome = models.CharField("nome", max_length=150)
     titulo = models.CharField("titulo profissional", max_length=150)
     subtitulo = models.CharField("subtitulo profissional", max_length=250, blank=True)
@@ -13,6 +18,12 @@ class Perfil(models.Model):
     class Meta:
         verbose_name = "perfil profissional"
         verbose_name_plural = "perfis profissionais"
+        constraints = [
+            models.CheckConstraint(
+                condition=models.Q(id=1),
+                name="portfolio_perfil_id_igual_1",
+            ),
+        ]
 
     def __str__(self):
         return self.nome
